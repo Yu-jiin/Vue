1af31bb8c20cda8c959da9b7fb04fff8165b8305
Yujiin <jiin4083@naver.com>
2024-11-07 09:35:41 +0900

    단방향 props

---
+++ 04-component-state-flow/vue-project/src/components/Parent.vue
@@ -0,0 +1,16 @@
+
+<template>
+<div>
+  <!-- Parent 에서 ParentChild에 보낼 props 작성 -->
+   <!-- my-msg(props이름) = "message"(props값) -->
+  <ParentChild my-msg="message"/>
+</div>
+</template>
+
+<script setup>
+import ParentChild from '@/components/ParentChild.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ 04-component-state-flow/vue-project/src/components/ParentChild.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script setup>
+// Parent에서 보낸 props 선언
+// 1. 문자열 배열을 사용한 선언
+defineProps(['myMsg'])
+// 2. 객체를 사용한 선언
+// defineProps({
+//   Mymsg: String
+// })
+</script>
+
+<style scoped>
+
+</style>
+  
