99fcb8cfa1b0069b60fd8657689e7e7cdbabfde4
Yujiin <jiin4083@naver.com>
2024-11-11 12:38:36 +0900

    live done

--- 05-routing/vue-project/src/router/index.js
+++ 05-routing/vue-project/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import UserView from '../views/UserView.vue'
+import UserPosts from '@/components/UserPosts.vue'
+import UserProfile from '@/components/UserProfile.vue'
+import LoginView from '@/views/LoginView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -18,8 +21,34 @@ const router = createRouter({
       path: '/user/:id',
       name: 'user',
       component: UserView,
+      children: [
+        { path: '', name: 'user', component: UserProfile },
+        { path: 'profile', name: 'user-profile', component: UserProfile },
+        { path: 'posts', name: 'user-posts', component: UserPosts },
+      ],
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+      beforeEnter: (to, from) => {
+        if (isAuthenticated === true) {
+          console.log('이미 로그인 상태입니다.')
+          return {name : 'home'}
+        }
+      }
     },
   ],
 })
+
+// router.beforeEach((to, from) => {
+//   console.log(to)
+//   console.log(from)
+//   // 로그인이 되어있지않고, 이동하고자 하는 페이지가 login 아니라면
+//   if(!isAuthenticated && to.name !== 'login') {
+//     console.log('로그인이 필요합니다.')
+//     return {name : 'login'}
+//   }
+// })
 
 export default router
--- 05-routing/vue-project/src/views/UserView.vue
+++ 05-routing/vue-project/src/views/UserView.vue
@@ -2,16 +2,40 @@
 <template>
   <div>
     <h1>UserView</h1>
+    <RouterLink :to="{ name: 'user-profile'}">Profile</RouterLink>
+    <RouterLink :to="{ name: 'user-posts'}">Posts</RouterLink>
     <h2>{{ $route.params.id }}번 User 페이지</h2>
     <h2>{{ userId }}번 User 페이지</h2>
+    <button @click="goHome">Home !</button>
+    <button @click="routeUpdate">100번 유저 페이지</button>
+    <hr>
+    <RouterView/>
   </div>
 </template>
 
 <script setup>
-import {ref} from 'vue'
-import { useRoute } from 'vue-router';
+import {onBeforeUpdate, ref} from 'vue'
+import { useRoute, RouterLink, RouterView, useRouter, onBeforeRouteLeave, onBeforeRouteUpdate } from 'vue-router';
 const route = useRoute()
 const userId = ref(route.params.id)
+const router = useRouter()
+const goHome = function() {
+  router.push({ name: 'home'})
+}
+onBeforeRouteLeave((to, from) => {
+  const answer = window.confirm('정말 떠나실건가요 ?')
+  if (answer===false) {
+    return false
+  }
+})
+
+const routeUpdate = function () {
+  router.push({ name:'user', params:{ id: 100 } })
+}
+
+onBeforeRouteUpdate((to, from) => {
+  userId.value = to.params.id
+})
 </script>
 
 <style scoped>
