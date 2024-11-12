a874ca87342e9a41948f1d988c12b6a422d242bc
Yujiin <jiin4083@naver.com>
2024-11-12 09:33:42 +0900

    pinia 구성요소 done

--- 06-state-management/vue-project/src/stores/counter.js
+++ 06-state-management/vue-project/src/stores/counter.js
@@ -4,8 +4,14 @@ import { defineStore } from 'pinia'
 // pinia counter.js = 중앙저장소
 //           defineStor()의 반환값 이름은 use와 store 사용 권장
 export const useCounterStore = defineStore('counter', () => {
+
+  // ref() === state
   const count = ref(0)
+
+  // computed() === getters
   const doubleCount = computed(() => count.value * 2)
+
+  // function() === actions
   function increment() {
     count.value++
   }
